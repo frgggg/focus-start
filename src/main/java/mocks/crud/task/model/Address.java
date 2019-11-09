01461d90c582c5697e1224ced4081d413e46566b
frgggg <zhar.feda@yandex.ru>
2019-11-09 10:59:16 +0300

    add get/set methods

--- src/main/java/mocks/crud/task/model/Address.java
+++ src/main/java/mocks/crud/task/model/Address.java
@@ -3,6 +3,28 @@ package mocks.crud.task.model;
 public class Address {
 
     private Long id;
-
     private String address;
+
+    void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Address(Long id, String address)
+    {
+        this.id = id;
+        this.address = address;
+    }
 }
